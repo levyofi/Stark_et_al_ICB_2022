eb254ecad3bc169b58f54e939c8decf759ccc568
Ofir Levy <levyofi@gmail.com>
2022-05-11 12:23:50 +0300

    Update Create_predictions_figure.R
    Cleaned up the code

--- data/Create_predictions_figure.R
+++ data/Create_predictions_figure.R
@@ -4,31 +4,7 @@ rescale <- function(x, to, from) {
   return(to[1] + x.temp*(to[2]-to[1]))
 }
 
-Data_bc <- read.csv("Fixed_maps/Rock_and_Veg_Cover_Results.2022-01-24.csv",header=T)
-#Convert season to from character to a factor: 
-Data_bc$Season <- as.factor(Data_bc$Season)
-Data_bc$BC = Data_bc$Mass..in.grams./Data_bc$SVL
-Data_bc = Data_bc[Data_bc$SVL>35,]
-Data_bc = na.omit(Data_bc)
-Data_bc = Data_bc[Data_bc$ID!="GS30",]
-Data_bc$logsvl = log(Data_bc$SVL)
-Data_bc$logmass = log(Data_bc$Mass..in.grams.)
-#The residual index of condition (Ri) -https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5349178/
-lm_model = lm(logmass~logsvl, data=Data_bc)
-#lm_model = lm(logmass~logsvl+Season, data=Data_bc)
-summary(lm_model)
-Data_bc$Ri = residuals(lm_model)
-#The scaled mass index (Mi) of condition
-pearson = cor(Data_bc$logmass, Data_bc$logsvl, method="pearson")
-mean_svl = mean(Data_bc$SVL)
-bSMA = 2.7555/pearson
-Data_bc$Mi = Data_bc$Mass..in.grams. * (mean_svl/Data_bc$SVL)^bSMA #Mi = M × (SVL0/SVL)bSMA
-
-Data_bc = Data_bc[Data_bc$Season %in% c("Summer", "Winter"),]
-nrow(Data_bc[Data_bc$Season=="Winter",])
-nrow(Data_bc[Data_bc$Season=="Summer",])
-nrow(Data_bc[Data_bc$Location=="Mishmar River",])
-nrow(Data_bc[Data_bc$Location!="Mishmar River",])
+Data_bc <- read.csv("lizards_Mi_data_Appendix_S1.csv",header=T)
 
 library(ggplot2)
 library(R2jags)
@@ -41,11 +17,6 @@ frock <-(original.frock - mean(Data_bc$frock10))/(2*sd(Data_bc$frock10))
 svl=0 #mean svl for the model (scaled!!)
 
 iseason=2; ifrock=3; ifveg=4; isvl = 5; ifvegrock=6
-# X[i,2]<-equals(season[i], 2)				
-# X[i,3]<-(rock[i]-mean(rock[]))/(2*sd(rock[]))
-# X[i,4]<-(veg[i]-mean(veg[]))/(2*sd(veg[]))
-# X[i,5]<-(svl[i]-mean(svl[]))/(2*sd(svl[]))
-# X[i,6]<-X[i,3]*X[i,4]
 
 beta.season = jagsfit$BUGSoutput$mean$alpha[2]
 beta.frock= jagsfit$BUGSoutput$mean$alpha[3]
@@ -68,10 +39,7 @@ Predicted.Mi.frock = array(dim=c(100, n.sims))
 Predicted.Mi.fveg.winter = array(dim=c(100, n.sims))
 Predicted.Mi.frock.winter = array(dim=c(100, n.sims))
 
-# Predicted.Mi.average_svl_for_frockveg_interaction = array(dim=c(100, 100))
-# Predicted.Mi.average_svl_for_frockveg_interaction.winter = array(dim=c(100, 100))
-
-# Fill in these vectors: this is clumsy, but it works
+# Fill in these vectors
 for(i in 1:100) {
   k=1
   for(j in (start_sim+1:n.sims)) {
@@ -81,10 +49,6 @@ for(i in 1:100) {
     Predicted.Mi.fveg.winter[i,k] = jagsfit$BUGSoutput$sims.list$alpha_mean[j,1]+ jagsfit$BUGSoutput$sims.list$alpha[j,ifveg]*fveg[i] + jagsfit$BUGSoutput$sims.list$alpha[j,iseason]
     Predicted.Mi.frock.winter[i,k] = jagsfit$BUGSoutput$sims.list$alpha_mean[j,1]+ jagsfit$BUGSoutput$sims.list$alpha[j,ifrock]*frock[i] + jagsfit$BUGSoutput$sims.list$alpha[j,iseason]
     k=k+1
-    # for (k in 1:100){
-    #   Predicted.Mi.average_svl_for_frockveg_interaction[i,k,j] = jagsfit$BUGSoutput$sims.list$alpha[j,1]+ jagsfit$BUGSoutput$sims.list$alpha[j,ifveg]*fveg[k] + jagsfit$BUGSoutput$sims.list$alpha[j,ifrock]*frock[i] + jagsfit$BUGSoutput$sims.list$alpha[j,ifvegrock]*frock[i]*fveg[k]
-    #   Predicted.Mi.average_svl_for_frockveg_interaction.winter[i,k,j] = jagsfit$BUGSoutput$sims.list$alpha[j,1]+ jagsfit$BUGSoutput$sims.list$alpha[j,ifveg]*fveg[k] + jagsfit$BUGSoutput$sims.list$alpha[j,ifrock]*frock[i] + jagsfit$BUGSoutput$sims.list$alpha[j,ifvegrock]*frock[i]*fveg[k] + jagsfit$BUGSoutput$sims.list$alpha[j,iseason]
-    # }
   }    
 }
 
@@ -101,8 +65,6 @@ UCB.winter.frock<-apply(Predicted.Mi.frock.winter, 1, quantile, prob=0.975)
 tiff(file="Figure 3.tiff", width=1600, height=2000, res=300, compression="lzw")
 par(mfcol=c(2,1), cex=1.1)
 par(mar=c(2,4,1,2))
-#par(bty="l")
-#fveg
 plot(c(0,max(original.fveg)), c(0,4), type="n", xlab="", ylab="", xaxt="n", yaxt="n")
 points(original.fveg, LCB.summer.fveg, col="orange", type="l", lwd=2, lty=2)
 points(original.fveg, UCB.summer.fveg, col="orange", type="l", lwd=2, lty=2)
@@ -116,11 +78,8 @@ axis(1, at = seq(0, 0.10, 0.01), cex.axis=0.8, lwd=0, line=-1, labels = seq(0, 0
 axis(1, at = seq(0, 0.10, 0.005), labels=F, tcl=-0.2)
 axis(2, at = seq(0, 4, 0.5), cex.axis=0.8, lwd=0, line=-0.5, las=2)
 axis(2, at = seq(0, 4, 0.25), labels=F, tcl=-0.2)
-#mtext(side=2, "Mi", line=1.6)
 mtext(side=1, "Vegetation cover (%)", line=1, cex=1.3)
 par(mar=c(2,4,1,2))
-#par(bty="l")
-#fveg
 plot(c(0,max(original.frock)), c(0,4), type="n", xlab="", ylab="", xaxt="n", yaxt="n")
 points(original.frock, LCB.summer.frock, col="orange", type="l", lwd=2, lty=2)
 points(original.frock, UCB.summer.frock, col="orange", type="l", lwd=2, lty=2)
