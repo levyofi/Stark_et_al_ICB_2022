5903b4defbd08c9ec09eda02f6be7181b3fe0ea2
Ofir Levy <levyofi@gmail.com>
2022-05-17 12:55:16 +0300

    Update Code for figure of ground temperature all year.R
    Added code to create the figure as a tif file with 300 DPI

--- Code/Code for figure of ground temperature all year.R
+++ Code/Code for figure of ground temperature all year.R
@@ -1,7 +1,9 @@
 ##################################Figure of ground temperature around the year######################
-combined_temps <- read.csv("combined_temps.csv") ### upload `combined_temps` dataset from Data folder###
+combined_temps <- read.csv("/combined_temps.csv") ### upload `combined_temps` dataset from Data folder###
 combined_temps$Month <- factor(combined_temps$Month, levels=unique(combined_temps$Month))
 months <- c("January", "February", "March","April","May","June","July","August","September","October","November","December")
+
+tiff(file="Figure 1.tiff", width=2500, height=1800, res=300, compression="lzw")
 o <- ggplot(combined_temps, aes(Month,mean_temp)) +
   geom_point(aes(color = Microhabitat), size=5) + 
   geom_line(aes(color = Microhabitat, group=Microhabitat))+  
@@ -14,10 +16,11 @@ o <- ggplot(combined_temps, aes(Month,mean_temp)) +
         ,axis.title.x=element_text(face='bold', size=15),
         axis.title.y=element_text(face='bold', size=15, vjust = 1))+scale_color_manual(values = c("orange","Black"))+
   scale_x_discrete(limits =c("1", "2", "3","4", "5", "6", "7","8", "9", "10","11", "12"),
-                   labels = c("1" = "January", "2" = "Feburary", "3" = "March",
-                              "4" = "April", "5" = "May", "6" = "June", "7" = "July",
-                              "8" = "August", "9" = "Spetember", "10" = "October",
-                              "11" = "November", "12" = "December"))+
+                   labels = c("1" = "Jan", "2" = "Feb", "3" = "Mar",
+                              "4" = "Apr", "5" = "May", "6" = "Jun", "7" = "Jul",
+                              "8" = "Aug", "9" = "Sep", "10" = "Oct",
+                              "11" = "Nov", "12" = "Dec"))+
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
         panel.background = element_blank(), axis.line = element_line(colour = "black"))
 o
+dev.off()
