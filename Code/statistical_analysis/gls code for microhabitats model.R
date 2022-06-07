81b1fc095f86d51579952dbef731350d38053699
levyofi <levyofi@gmail.com>
2022-06-07 12:19:21 +0300

    added gls code

---
+++ Code/statistical_analysis/gls code for microhabitats model.R
@@ -0,0 +1,13 @@
+combined_temps <- read.csv("Data/combined_temps_final.csv") # upload dataset
+combined_temps <- combined_temps[combined_temps$Season %in% c("Summer", "Winter"),]
+str(combined_temps) # check that the structure of the table fits the Anova
+
+library(nlme)
+gls.model <- gls(mean_temp ~ Microhabitat*Season, data = combined_temps, weights = varIdent(form =~1|Microhabitat*Season)) # run Two-Way Anova
+
+summary(gls.model)
+
+e = residuals(gls.model, type="n")
+plot(e)
+boxplot(e~combined_temps$Season+combined_temps$Microhabitat)
+plot(e~combined_temps$mean_temp)
